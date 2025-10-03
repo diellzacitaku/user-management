6ba82e2c4b57258a8ba853b45a874404987026d4
Diellza <diellza.i.citaku@gmail.com>
2025-10-03 22:36:07 +0200

    add styles to SearchBar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,16 +1,15 @@
-import { useEffect, useState } from 'react';
+import './style.css'
 
 export default function SearchBar({ value, onChange, onReset, disabled = false }){
-    const [query, setQuery] = useState('');
 
     return <>
      <input
+     className='input search'
         type="search"
         placeholder="Search by name or email"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         disabled={disabled}
-        style={{ padding: 8, width: 300, marginBottom: 12 }}
         aria-label="Search users"
       />
     </>
